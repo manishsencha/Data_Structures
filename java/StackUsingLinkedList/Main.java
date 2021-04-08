39d2d4b1648d3c59e7b29de5ea742bc8ca1d2978
Manish Sencha <manishsencha2001@gmail.com>
2021-04-09 00:15:56 +0530

    LinkedList implementation of Stack

---
+++ java/StackUsingLinkedList/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        StackList st = new StackList();
+        for (int i = 0; i < 5; ++i) {
+            st.push(new Random().nextInt(1000));
+            System.out.println(st.peek());
+        }
+        System.out.println(st.isEmpty());
+
+        System.out.println(st.peek());
+
+        System.out.println();
+        while (!st.isEmpty()) {
+            System.out.println(st.peek());
+            st.pop();
+        }
+    }
+}
---
+++ java/StackUsingLinkedList/StackList.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+
+// Class for stack
+public class StackList {
+    private LinkedList<Integer> llist = new LinkedList<Integer>();
+
+    // Method to push Integer into Stack and return the status
+    boolean push(int x) {
+        try {
+
+            llist.add(x);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Method to get the top element of the Stack
+    int peek() {
+        try {
+            return llist.getLast();
+        } catch (Exception e) {
+            System.out.println("Stack is empty!!");
+            return -1;
+        }
+    }
+
+    // Method to pop the top element from the stack
+    boolean pop() {
+        try {
+            llist.removeLast();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Stack is empty!!");
+            return false;
+        }
+    }
+
+    // Method to check if the stack is empty
+    boolean isEmpty() {
+        return llist.isEmpty();
+    }
+
+}
